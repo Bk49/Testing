80d3425fa29cebd6446cc00d9796d0878f0773a2
Bk49 <ericngyongwei@gmail.com>
2024-03-03 00:04:47 +0800

    Add API KEY INSIDE

--- api.ts
+++ api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const whateverFunc = async () => {
-    return await "Hello World";
+    const result = await axios.get("https://api.api-ninjas.com/v1/airports", {headers: {
+        "X-Api-Key": "YOURAPIKEY" 
+    }});
+
+    return result.data;
 };
 
 whateverFunc()
